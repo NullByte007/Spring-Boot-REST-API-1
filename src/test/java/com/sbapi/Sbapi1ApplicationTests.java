f689dcad5c7c044e8661fc7170db24f42cbd9524
NullByte007 <aniketbhagwate@gmail.com>
2021-09-21 16:21:44 +0530

    First commit

---
+++ src/test/java/com/sbapi/Sbapi1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sbapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbapi1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
